1cb606fd0d0029bbcec4384f59905f8dcc9e1959
DmitrySergienko <dmitrisergienko@gmail.com>
2022-06-14 14:07:38 +0400

    Initial commit

---
+++ app/src/main/java/com/example/coroutinesflow/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.coroutinesflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coroutinesflow.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
